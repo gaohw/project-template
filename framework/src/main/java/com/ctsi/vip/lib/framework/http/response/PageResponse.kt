73cb9f7f30c292cc495b5754b6aec8a4365c9d72
gaohw <1157687744@qq.com>
2022-11-03 14:29:28 +0800

    fix

---
+++ framework/src/main/java/com/ctsi/vip/lib/framework/http/response/PageResponse.kt
@@ -0,0 +1,19 @@
+package com.ctsi.vip.lib.framework.http.response
+
+/**
+ * Class : PageResponse
+ * Create by GaoHW at 2022-11-3 14:23.
+ * Description: 分页查询返回体封装
+ */
+class PageResponse<T> {
+
+    constructor()
+
+    constructor(page: Int) {
+        this.page = page
+    }
+
+    var data: List<T>? = null
+    var page: Int = 0
+    var total: Int = 0
+}
