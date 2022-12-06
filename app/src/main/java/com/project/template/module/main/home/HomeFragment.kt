fc4bd99370469d86043d5d2141ce6781845a125a
gaohw <1157687744@qq.com>
2022-12-06 20:19:41 +0800

    图片查看

--- app/src/main/java/com/project/template/module/main/home/HomeFragment.kt
+++ app/src/main/java/com/project/template/module/main/home/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.project.template.module.main.home
 
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import cc.shinichi.library.ImagePreview
 import com.blankj.utilcode.util.LogUtils
 import com.ctsi.vip.lib.framework.base.BaseFragment
 import com.donkingliang.imageselector.utils.ImageSelector
@@ -23,7 +24,7 @@ class HomeFragment : BaseFragment<MainViewModel, FragmentHomeBinding>() {
         mBinding.vTitle.showBack(false).setTitleText("首页").setTitleLightMode(false).fitSystemWindows(true)
             .setTitleBackground(R.color.color_main)
 
-        mBinding.btnTest.setOnClickListener {
+        mBinding.btnImageSelect.setOnClickListener {
             ImageSelector.builder()
                 .setSingle(true).setCrop(true)
                 .start(this, REQUEST_IMAGE); // 打开相册
@@ -33,7 +34,9 @@ class HomeFragment : BaseFragment<MainViewModel, FragmentHomeBinding>() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_IMAGE && resultCode == RESULT_OK) {
             data?.getStringArrayListExtra(ImageSelector.SELECT_RESULT)?.let {
-                LogUtils.e(it)
+                ImagePreview.getInstance().setContext(requireContext())
+                    .setImageList(it).setShowDownButton(false)
+                    .start()
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data)
